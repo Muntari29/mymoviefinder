57c2d3ebab9fbfbc6c482aaca9eacd3501b4bb8d
Muntari29 <muntrock@gmail.com>
2022-01-19 13:46:33 +0900

    add: 브레이크포인트 & 색상 constants 파일 추가

---
+++ utils/constants/colors.tsx
@@ -0,0 +1,18 @@
+interface IColors {
+  [color: string]: string;
+}
+
+export const COLORS: IColors = {
+  BLACK: '#000000',
+  WHITE: '#ffffff',
+  TEXT_GRAY_LIGHT: '#BEBEBE',
+  TEXT_GRAY_DARK: '#757575',
+  TEXT_BLACK: '#2b2b2b',
+  BG_YELLOW: '#FBF2D2',
+  BG_ORANGE: '#FBD2D2',
+  BG_PINK: '#FBD2F7',
+  BG_BLUE: '#D2FBF8',
+  BG_GREEN: '#E4FBD2',
+  BG_PURPLE: '#E4D2FB',
+  BG_GRAY: '#dddddd',
+};
