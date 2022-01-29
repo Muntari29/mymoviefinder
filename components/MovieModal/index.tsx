cefe3328a6561dc811651fc7f61f9056ded07afd
Muntari29 <muntrock@gmail.com>
2022-01-30 01:45:36 +0900

    feat: MovieModal 컴포넌트 ResizeImage 300 -> 700

--- components/MovieModal/index.tsx
+++ components/MovieModal/index.tsx
@@ -1,6 +1,7 @@
 import { getDetailMovieData } from '@/pages/api/movie';
 import { IgetOneMovieData, IMovieModal } from '@/utils/interfaces/movies';
 import { useCallback, useEffect, useState } from 'react';
+import Image from 'next/image';
 import style from './index.module.scss';
 
 const MovieModal = ({ seletedMovieId, onClose }: IMovieModal): JSX.Element => {
@@ -9,13 +10,22 @@ const MovieModal = ({ seletedMovieId, onClose }: IMovieModal): JSX.Element => {
 
   const initModal = useCallback(async () => {
     const data = await getDetailMovieData(seletedMovieId);
-    setGetOneMovieData(data);
-    console.log(data);
+    reszieImage(data);
   }, [seletedMovieId]);
 
   useEffect(() => {
     initModal();
   }, [initModal]);
+
+  const reszieImage = (data: IgetOneMovieData) => {
+    const posterArray = data.Poster.split('_');
+    const sizeIndex = posterArray.indexOf('SX300.jpg');
+    posterArray[sizeIndex] = 'SX700.jpg';
+    const resizePosterUrl = posterArray.join('_');
+    data.Poster = resizePosterUrl;
+    setGetOneMovieData(data);
+    console.log(data);
+  };
 
   // 추후 hook으로 분리하기.
   const handleModal = useCallback(
@@ -34,7 +44,16 @@ const MovieModal = ({ seletedMovieId, onClose }: IMovieModal): JSX.Element => {
 
   return (
     <div className={style.mask}>
-      <div className={style.wrapper}>{seletedMovieId}</div>
+      {getOneMovieData && (
+        <div className={style.wrapper}>
+          <Image
+            src={getOneMovieData.Poster}
+            width={'100%'}
+            height={'100%'}
+            alt={'Poster...'}
+          />
+        </div>
+      )}
     </div>
   );
 };
